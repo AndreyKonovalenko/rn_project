98bf0905f192fc56a2800d050fae230f7a0fef97
AndreyKonovalenko <andreinew@mail.ru>
2019-07-29 21:35:37 +0300

    Set up configureStore.jx file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+// the place above is for extra configuration
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+AppRegistry.registerComponent(appName, () => RNRedux);
